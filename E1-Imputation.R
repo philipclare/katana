ac424eb89839cc3e9294f85ef5d73db89139c613
philipclare <pjclare@gmail.com>
2020-07-01 15:11:12 +1000

    Add code files

---
+++ E1-Imputation.R
@@ -0,0 +1,62 @@
+##############################################################################
+##   
+## Katana example 1
+## Multiple imputation using 'parlmice'
+## Date: 30 June 2020
+##
+##############################################################################
+# 1. Setup Environment
+#-----------------------------------------------------------------------------
+
+# 1.1 Specify paths
+.libPaths("C:/Users/pjclare/Documents/My Dropbox/R Library") # R library path
+workdir <- "C:/Users/pjclare/Cloudstor/Stats/Topic 6 - Katana/"
+# .libPaths("/home/z3312911/RPackages")
+# workdir <- "/home/z3312911/methodssim/"
+
+# 1.2 Check libraries, install missing packages, update old packages, and then load required packages
+libs <- c("mice","miceadds","parallel")
+install <- !libs %in% installed.packages() | libs %in% old.packages()
+if (any(install)) {
+  install.packages(libs[install])
+}
+lapply(libs, library, character.only = TRUE)
+
+# 1.3 Set seed
+set.seed(56188)
+
+##############################################################################
+# 2.Load data
+#-----------------------------------------------------------------------------
+
+data(nhanes)
+
+##############################################################################
+# 3. Define Imputation Paramaters
+#-----------------------------------------------------------------------------
+
+m <- 40 # Number of imputations
+maxit <- 100; # Number of mice iterations
+cluster.seed <- 23771 # Needs to be set within function for parallel computing
+numcores <- as.numeric(Sys.getenv('NCPUS')) # Defined from cluster environment variable
+n.imp.core = m/numcores # number of imputations per core
+cl.type = "FORK" # Could be PSOCK for Windows based systems
+
+##############################################################################
+# 4. Imputation
+#-----------------------------------------------------------------------------
+
+# 3.3 Parallel imputation using parlmice and randomforests
+imp_mice <- parlmice(data=nhanes,
+                     cluster.seed=cluster.seed,
+                     n.core=numcores,
+                     n.imp.core=n.imp.core,
+                     maxit=maxit,
+                     method="rf",
+                     cl.type=cl.type)
+imp_mice <- mids2datlist(imp_mice)
+
+##############################################################################
+# 5. Save
+# 5.1 Save imputation
+save(imp_mice, file=paste0(workdir,"imputed-mice.RData"))
---
+++ E2-Monte-Carlo.R
@@ -0,0 +1,104 @@
+##############################################################################
+##   
+## Katana example 2
+## Simple Monte-carlo Simulation
+## Date: 30 June 2020
+##
+##############################################################################
+# 1. Setup Environment
+#-----------------------------------------------------------------------------
+
+# 1.1 Specify paths
+.libPaths("C:/Users/pjclare/Documents/My Dropbox/R Library") # R library path
+workdir <- "C:/Users/pjclare/Cloudstor/Stats/Topic 6 - Katana/"
+# .libPaths("/home/z3312911/RPackages")
+# workdir <- "/home/z3312911/methodssim/"
+
+# 1.2 Check libraries, install missing packages, update old packages, and then load required packages
+libs <- c("dqrng","lme4","dplyr","tidyverse")
+install <- !libs %in% installed.packages() | libs %in% old.packages()
+if (any(install)) {
+  install.packages(libs[install])
+}
+if (!"faux" %in% installed.packages()) {
+  devtools::install_github("debruine/faux")
+}
+lapply(libs, library, character.only = TRUE)
+library("faux")
+
+# 1.3 Define arguments passed through to R from Katana
+# stream <- commandArgs(trailingOnly = TRUE)
+stream <- 1
+
+# 1.4 Set seed and stream
+dqset.seed(56418,stream)
+
+##############################################################################
+# 2. Simulate data
+#-----------------------------------------------------------------------------
+
+# 2.1 Define simulation function
+# set up the custom data simulation function
+sim_data <- function(
+  nsubj  = 100, # number of subjects
+  nitem  = c(ingroup = 25, outgroup = 25),  # number of items
+  b0     = 800, # grand mean
+  b1     =  50, # effect of category
+  I0i_sd =  80, # by-item random intercept sd
+  S0s_sd = 100, # by-subject random intercept sd
+  S1s_sd =  40, # by-subject random slope sd
+  scor   = 0.2, # correlation between intercept and slope
+  err_sd = 200){  # residual (standard deviation)
+  
+  # simulate items
+  items <- faux::sim_design(
+    between = list(category = c("ingroup", "outgroup")),
+    n = nitem,
+    sd = I0i_sd,
+    dv = "I0i",
+    id = "item_id",
+    plot = FALSE
+  )
+  
+  # effect code category
+  items$cat <- recode(items$category, "ingroup" = -0.5, "outgroup" = 0.5)
+  
+  # simulate subjects
+  subjects <- faux::sim_design(
+    within = list(effect = c(S0s = "By-subject random intercepts", 
+                             S1s = "By-subject random slopes")), 
+    n = nsubj,
+    sd = c(S0s_sd, S1s_sd), 
+    r = scor,
+    id = "subj_id",
+    plot = FALSE
+  )
+  
+  # simulate trials
+  dat_sim <- crossing(subj_id = subjects$subj_id,
+                      item_id = items$item_id) %>%
+    inner_join(subjects, "subj_id") %>%
+    inner_join(items, "item_id") %>%
+    mutate(err = rnorm(nrow(.), mean = 0, sd = err_sd)) %>%
+    mutate(RT = b0 + I0i + S0s + (b1 + S1s) * cat + err) %>%
+    select(subj_id, item_id, category, cat, RT)
+  
+  dat_sim
+}
+
+sim_res <- matrix(unlist(lapply(rep(1, 1000), function (x) {
+  dat_sim <- sim_data()
+  mod_sim <- lmer(RT ~ 1 + cat + (1 | item_id) + (1 + cat | subj_id),
+                  data = dat_sim,
+                  REML = FALSE)
+  results <- coef(summary(mod_sim))["cat",]
+  results[1:2]
+})),ncol=2,byrow=TRUE)
+
+sim_res <- lapply(rep(1, 10), simulate(x))
+
+##############################################################################
+# 3. Save results to be combined later
+#-----------------------------------------------------------------------------
+
+save(sim_res,file=paste0(workdir,"Sim-res-",stream,".RData",collapse=""))
